a5ecd861986c02d3647eaacb5cd5ca22884fb264
Nathan Zadoks <nathan@nathan7.eu>
2014-04-11 14:11:42 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,75 @@
+'use strict'; //jshint node:true
+module.exports = Model
+var Scuttlebutt = require('scuttlebutt')
+  , inherits = require('util').inherits
+  , eq = require('is-equal')
+
+inherits(Model, Scuttlebutt)
+function Model(opts) {
+  if (!this || this === global) return new Model(opts)
+  Scuttlebutt.call(this, opts)
+  this._history = []
+}
+
+var m = Model.prototype
+
+m._set = function(path, value) {
+  this.localUpdate([path, value])
+}
+
+m.applyUpdate = function(message) {
+  this._history = this._history
+    .concat([message])
+    .sort(byTimestamp)
+    .reduce(function(history, freshMessage) {
+      console.log(freshMessage)
+      // freshUpdate = [[a], b]
+      // invalidates anything with [[a, ..], b]
+      var freshUpdate = freshMessage[0]
+      return history
+        .filter(function(message) {
+          var update = message[0]
+          return startsWith(freshUpdate[0], update[0])
+        })
+        .concat([freshMessage])
+    }, [])
+  
+  return true
+}
+
+m.history = function(sources) {
+  return this._history
+    .filter(function(message) {
+      var ts = message[1]
+        , source = message[2]
+      return (!sources || !sources[source] || sources[source] < ts)
+    })
+}
+
+m.toJSON = function() {
+  return this._history
+    .reduce(function(obj, message) {
+      var update = message[0]
+      return assocIn(obj, update[0], update[1])
+    }, {})
+}
+
+function assocIn(obj, keys, value) {
+  var key = keys.shift()
+  return keys.length
+    ? assoc(obj, key, assoc(obj[key] || {}, keys, value))
+    : assoc(obj, key, value)
+}
+
+function assoc(obj, key, value) {
+  var ret = {}
+  Object.keys(obj).forEach(function(key) { ret[key] = obj[key] })
+  ret[key] = value
+  return ret
+}
+
+function byTimestamp(a, b) {
+  return a[1] - b[1] || (a[2] > b[2] ? 1 : -1)
+}
+
+function startsWith(prefix, value) { return eq(prefix, value.slice(0, prefix.length)) }
